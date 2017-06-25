ba4d01bd8ee8f9f6bf9ff61c23673af423f13175
Logan Saso <logan.saso2016@gmail.com>
2017-06-25 11:35:16 -0700

    Verified working

--- Program.cs
+++ Program.cs
@@ -18,7 +18,6 @@ namespace PhoneCallScript
 			Console.WriteLine(" -- Call Scammer Line Flooder (v0.0.1) -- "); //Fuck yeah, semantic versioning
 			Console.Write("Enter number to flood harder than the red sea in egypt (remember to add +1 at the beginning): ");
 			NumToCall = Console.ReadLine();
-			Console.WriteLine("");
 			Console.WriteLine("Press ENTER to let the sea walls fall. Otherwise, hit the X to close.");
 			Console.ReadLine();
 			Console.Clear();
@@ -31,10 +30,10 @@ namespace PhoneCallScript
 				foreach (string num in numbers)
 				{
 					Call(num);
-					System.Threading.Thread.Sleep(1000);
+					System.Threading.Thread.Sleep(10000);
 				}
 				count++;
-				System.Threading.Thread.Sleep(5000);
+				System.Threading.Thread.Sleep(50000);
 			} while (true); //Dangerous game here, scotty
 		}
 
@@ -46,7 +45,7 @@ namespace PhoneCallScript
 					to: new PhoneNumber(NumToCall),
 					from: new PhoneNumber(fromNumber),
 					record: true,
-					url: new Uri("Some info on what to do when answering.") //Should be a link to this kind of file: https://www.twilio.com/docs/api/twiml
+					url: new Uri("") //Should be a link to this kind of file: https://www.twilio.com/docs/api/twiml - Host here: https://www.twilio.com/console/dev-tools/twiml-bins
 				);
 				Console.WriteLine($"Starting call to {call.To} from {fromNumber}.");
 			}
