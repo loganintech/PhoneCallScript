3aeea5c00f271e82d8cb98785f00cb2b6da590b9
Logan Saso <logan.saso2016@gmail.com>
2017-06-26 01:49:07 -0700

    Added way to clean-exit loop instead of keyboard interrupt

--- Program.cs
+++ Program.cs
@@ -8,21 +8,21 @@ namespace PhoneCallScript
 {
 	class Program
 	{
-		public static string accountId = ""; //Can be accessed from: https://www.twilio.com/console
-		public static string authToken = ""; //Can be accessed from: https://www.twilio.com/console
-		public static List<string> numbers = new List<string>(new string[] { "+1", "+1" }); //Get numbers from the twilio API https://www.twilio.com/console/phone-numbers/search (one dollar each)		public static List<string> numbersInUse = new List<string>(); //Idk why the guy included this, it was never used
+		public static string accountId = ""; // Can be accessed from: https://www.twilio.com/console
+		public static string authToken = ""; // Can be accessed from: https://www.twilio.com/console
+		public static List<string> numbers = new List<string>(new string[] { "+1", "+1" }); // Get numbers from the twilio API https://www.twilio.com/console/phone-numbers/search (one dollar each)		public static List<string> numbersInUse = new List<string>(); //Idk why the guy included this, it was never used
 		public static string NumToCall = "";
 		static void Main(string[] args)
 		{
-			Console.WriteLine(" -- Call Scammer Line Flooder (v0.0.1) -- "); //Fuck yeah, semantic versioning
+			Console.WriteLine(" -- Call Scammer Line Flooder (v0.0.1) -- "); // Fuck yeah, semantic versioning
 			Console.Write("Enter number to flood harder than the red sea in egypt (remember to add +1 at the beginning): ");
 			NumToCall = Console.ReadLine();
-			Console.WriteLine("Press ENTER to let the sea walls fall. Otherwise, hit the X to close.");
+			Console.WriteLine("Press ENTER to let the sea walls fall. Otherwise, hit the X to close. Hold Escape after a batch to clean exit the program, with stats.");
 			Console.ReadLine();
 			Console.Clear();
 			TwilioClient.Init(accountId, authToken);
 
-			int count = 0;
+			int count = 0, call_num = 0;
 			do
 			{
 				Console.WriteLine($"Starting Call Batch {count + 1} ({numbers.Count} Nums.)");
@@ -30,10 +30,12 @@ namespace PhoneCallScript
 				{
 					Call(num);
 					System.Threading.Thread.Sleep(1000);
+					call_num++;
 				}
 				count++;
 				System.Threading.Thread.Sleep(5000);
-			} while (true); //Dangerous game here, scotty
+			} while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape)); // Hold down the escape key to stop calling
+			Console.WriteLine($"Batches: {count + 1}\nCalls: {call_num + 1}\nView Recordings: https://www.twilio.com/console/voice/dashboard");
 		}
 
 		static void Call(string fromNumber)
@@ -43,8 +45,8 @@ namespace PhoneCallScript
 				var call = CallResource.Create(
 					to: new PhoneNumber(NumToCall),
 					from: new PhoneNumber(fromNumber),
-					record: true, //You can access the recordings from: https://www.twilio.com/console/voice/dashboard
-					url: new Uri("") //Should be a link to this kind of file: https://www.twilio.com/docs/api/twiml - Host here: https://www.twilio.com/console/dev-tools/twiml-bins
+					record: true, // You can access the recordings from: https://www.twilio.com/console/voice/dashboard
+					url: new Uri("") // Should be a link to this kind of file: https://www.twilio.com/docs/api/twiml - Host here: https://www.twilio.com/console/dev-tools/twiml-bins
 				);
 				Console.WriteLine($"Starting call to {call.To} from {fromNumber} starting {call.StartTime}.");
 			}
